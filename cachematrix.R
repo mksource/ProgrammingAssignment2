cd6c6c0643ce07f6b410cdb6d3a0b1e494c82748
mknarayan1711@gmail.com <mknarayan1711@hotmail.com>
2014-07-27 20:33:09 +0530

    Adding comments to cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -5,12 +5,20 @@
 
 makeCacheMatrix <- function(x = matrix()) {
       inverse <- NULL
+      
+       ##Set the matrix
        set <- function(y){
             x <<- y
             inverse <<- NULL
        }
+       
+       ##Get the matrix
        get <- function() x
+      
+       ##Set the inverse of the matrix
        setinverse <- function(inv) inverse <<- inv
+      
+       ##Get the inverse of the matrix
        getinverse <- function() inverse
   list(set = set,get = get, setinverse = setinverse
        ,getinverse = getinverse)
@@ -22,6 +30,8 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inverse <- x$getinverse()
+        
+        ##is the inverse is not null then return cached value
         if(!is.null(inverse)){
              message("getting cached data")
              return(inverse)
